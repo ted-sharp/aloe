1ca2a49917a311099d7b03b1f249dc3eee2d3e3c
ted <ted.sharp.flat@gmail.com>
2025-12-25 00:04:43 +0900

    Aloe.Apps.MedockClient を追加

---
+++ src/Aloe/Apps/Medock/Aloe.Apps.MedockClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Web.WebView2.Core;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aloe.Apps.MedockClient;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        await this.InitializeWebViewAsync();
+    }
+
+    private async Task InitializeWebViewAsync()
+    {
+        var env = await CoreWebView2Environment.CreateAsync();
+        await this.WebView.EnsureCoreWebView2Async(env);
+        this.WebView.Source = new Uri("http://localhost:5084");
+    }
+}
